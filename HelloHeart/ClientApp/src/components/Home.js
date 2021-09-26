337a6fca6cef2d9176e21363d7899c23f43a81af
Gal Gur <gal.gurevich@gmail.com>
2021-09-26 14:37:58 +0300

    frotnent input fields added require protection

--- HelloHeart/ClientApp/src/components/Home.js
+++ HelloHeart/ClientApp/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { regValidate, enumToText } from '../Helpers/stringHelper'
 import './Home.css'
 
@@ -8,15 +8,14 @@ export default function Home() {
   const [testNumber, setTestNumber] = useState("");
   const [bloodTestResult, setBloodTestResult] = useState(null);
   const [loading, setLoading] = useState(false);
-  const inputError = testInput && testInput.length > 1 && !regValidate(testInput);
+  const textInputError = testInput && testInput.length > 1 && !regValidate(testInput);
 
   const header = `Hello Heart`
-  const errorTxt = `Please avoid typing symbols, try again`
+  const errorTxt = `Please avoid typing symbols`
   const loaderTxt = `Calculating Results Please Wait...`;
 
   function onSubmit(e) {
     e.preventDefault();
-    if(!testInput || !testNumber) return;
     if(!regValidate(testInput)) return;
     submitBloodTestData(testInput, testNumber)
     resetStates();
@@ -60,13 +59,13 @@ export default function Home() {
       
       <form className="input-test-form" onSubmit={onSubmit}>
         <label className="label-header">Testing Type: </label>
-        <input className="input-field" type='text'value={testInput} onChange={e => setTestInput(e.target.value)} placeholder="what should we check today?"></input>
+        <input className="input-field" type='text'value={testInput} onChange={e => setTestInput(e.target.value)} placeholder="what should we check today?" required={true}></input>
         <label className="label-header">Testing Numiric Result: </label>
-        <input className="input-field" type='number' value={testNumber} onChange={e => setTestNumber(e.target.value)} placeholder={0}></input>
+        <input className="input-field" type='number' value={testNumber} onChange={e => setTestNumber(e.target.value)} placeholder={0} required={true}></input>
         <button type='submit'>Submit Result</button>
       </form>
 
-      {inputError && <span className="error-line">{errorTxt}</span>}
+      {textInputError && <span className="error-line">{errorTxt}</span>}
       <span className="heart"></span>
       { loading && <div className="loader">{loaderTxt}</div>}
       { bloodTestResult && <div className="result-test-txt">{renderOutputStr(bloodTestResult.result, bloodTestResult.resultEvaluation)}</div>}
